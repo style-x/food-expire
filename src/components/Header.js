848a40332232740d754b0ee8ffda2ad678980015
xan <47989552+style-x@users.noreply.github.com>
2021-04-14 18:32:43 +0200

    Add JS Files: Button, Header
    'Button' and 'Header' JS Files addet and code some

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 // import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = (props) => {
+const Header = ({ title }) => {
   return (
     <header className='header'>
       <h1>Food-EXP</h1>
+      <h2>{title}</h2>
+      <Button color='green' text='sort' />
+      <Button color='blue' text='ADD' />
     </header>
   )
+}
+
+Header.defaultProps = {
+  title: 'Food Expire',
 }
 
 export default Header
