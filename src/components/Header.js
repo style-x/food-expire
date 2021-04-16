7aeafcaa20e0ab8e0aa01f54f59c1e150f8c2ead
xan <47989552+style-x@users.noreply.github.com>
2021-04-16 18:10:08 +0200

    add json-server

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,6 @@ import Button from './Button'
 const Header = ({ title }) => {
   return (
     <header className='header'>
-      <h1>Food-EXP</h1>
-      <h2>{title}</h2>
       <Button color='green' text='sort' />
       <Button color='blue' text='ADD' />
     </header>
