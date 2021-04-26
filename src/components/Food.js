0a08ad9c15d9f28d41f2b5648fb0155c55196707
xan <47989552+style-x@users.noreply.github.com>
2021-04-26 19:31:00 +0200

    some... PRIVATE
    nur kleine Änderungen.
    Aber das Repo mal besser auf PRIVAT gestellt

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
 import { Container } from '@material-ui/core'
 import FoodCard from './FoodCard'
 
